f61fa6e6da8ef68d36d01aa5ea5635c87d91f203
makiroggers <makiroggers@gmail.com>
2017-10-08 12:36:12 -0700

    Fixed views

--- watpi/apps/dashboard/views.py
+++ watpi/apps/dashboard/views.py
@@ -1,15 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-<<<<<<< HEAD
 from django.shortcuts import HttpResponse, redirect, render
 
 
 def index(request):
     response = "Placeholder to verify dashboard app creation."
     return HttpResponse(response)
-=======
-from django.shortcuts import render
-
-# Create your views here.
->>>>>>> 6562496e92910fc316f8846bdd392898232adc3f
