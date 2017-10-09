093707d4cb65d49fe177817debf859badf3afe3e
makiroggers <makiroggers@gmail.com>
2017-10-08 20:20:24 -0700

    Update commands.

--- watpi/apps/dashboard/camera.py
+++ watpi/apps/dashboard/camera.py
@@ -43,37 +43,39 @@ import json
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
 
-def takephoto():
-    camera = picamera.PiCamera()
-    camera.resolution = (1024, 768)
-    camera.start_preview()
-    # Camera warm-up time
-    sleep(2)
-    camera.capture('image.jpg', resize=(320, 240))
 
-def main():
-    takephoto() # First take a picture
-    """Run a label request on a single image"""
+class FindImageLabels:
 
-    credentials = GoogleCredentials.get_application_default()
-    service = discovery.build('vision', 'v1', credentials=credentials)
+    def takephoto():
+        camera = picamera.PiCamera()
+        camera.resolution = (1024, 768)
+        camera.start_preview()
+        # Camera warm-up time
+        sleep(2)
+        camera.capture('image.jpg', resize=(320, 240))
 
-    with open('image.jpg', 'rb') as image:
-        image_content = base64.b64encode(image.read())
-        service_request = service.images().annotate(body={
-            'requests': [{
-                'image': {
-                    'content': image_content.decode('UTF-8')
-                },
-                'features': [{
-                    'type': 'LABEL_DETECTION',
-                    'maxResults': 10
+    def main():
+        takephoto() # First take a picture
+        """Run a label request on a single image"""
+
+        credentials = GoogleCredentials.get_application_default()
+        service = discovery.build('vision', 'v1', credentials=credentials)
+
+        with open('image.jpg', 'rb') as image:
+            image_content = base64.b64encode(image.read())
+            service_request = service.images().annotate(body={
+                'requests': [{
+                    'image': {
+                        'content': image_content.decode('UTF-8')
+                    },
+                    'features': [{
+                        'type': 'LABEL_DETECTION',
+                        'maxResults': 10
+                    }]
                 }]
-            }]
-        })
-        response = service_request.execute()
-        print json.dumps(response, indent=4, sort_keys=True)	#Print it out and make it somewhat pretty.
+            })
+            response = service_request.execute()
+            print json.dumps(response, indent=4, sort_keys=True)	#Print it out and make it somewhat pretty.
 
 if __name__ == '__main__':
-
-    main()
+    FindImageLabels.main()
