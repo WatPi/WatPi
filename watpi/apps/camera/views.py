d85796aae1e37b0b76c8316ad72949f8f0966e1b
makiroggers <makiroggers@gmail.com>
2017-10-09 20:08:54 -0700

    Cleaned up old code.

--- watpi/apps/camera/views.py
+++ watpi/apps/camera/views.py
@@ -8,7 +8,6 @@ import picamera
 
 
 def index(request):
-    # camera = picamera.PiCamera(resolution=(1024, 768))
     response = "Placeholder to verify rover app creation."
     context = {'response': response, 
                'image': 'images/image2.jpg', 
@@ -17,12 +16,10 @@ def index(request):
 
 
 def snap_photo(request, camera=picamera.PiCamera(resolution=(1024, 768))):
-    # camera = picamera.PiCamera(resolution=(1024, 768))
     image = camera.capture('apps/camera/static/images/image2.jpg', resize=(320, 240))
     context = {'image': 'images/image2.jpg',
                'caption': 'Here is your image!', }
     return redirect(reverse('camera:index'))
-    # return render(request, 'camera_index.html', context=context)
 
 
 def snap_lg_photo(request):
