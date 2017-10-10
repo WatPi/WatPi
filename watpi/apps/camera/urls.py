8862c17a6f68cda53aae98a7952d6ed52ae0eea6
makiroggers <makiroggers@gmail.com>
2017-10-10 02:50:17 -0700

    Update files.

--- watpi/apps/camera/urls.py
+++ watpi/apps/camera/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     # url(r'^take_photo/$', views.snap_photo, name='take_photo'),
     url(r'^take_lg_photo/$', views.snap_lg_photo, name='take_lg_photo'),
-    # url(r'^take_video/$', views.capture_10s_video, name='take_video'),
+    url(r'^take_video/$', views.capture_10s_video, name='take_video'),
     # url(r'^start_preview/$', views.video_preview, name='start_preview'),
 ]
--- watpi/apps/camera/views.py
+++ watpi/apps/camera/views.py
@@ -23,19 +23,20 @@ def index(request):
 
 
 def snap_lg_photo(request, camera=picamera.PiCamera(resolution=(1024, 768))):
-    image = camera.capture('image.jpg', resize=(800, 600))
-    context = {'image': image,
+    image = camera.capture('apps/camera/static/images/image2.jpg', resize=(800, 600))
+    context = {'image': 'images/image2.jpg',
                'caption': 'Here is your smaller image!', }
+    sleep(2)
     return redirect(reverse('camera:index'))
 
 
-# def capture_10s_video(request, camera=picamera.PiCamera(resolution=(1024, 768))):
-#     video = camera.start_recording('video.h264')
-#     sleep(10)
-#     video.stop_recording()
-#     context = {'video': video,
-#                'caption': 'Here is your video clip!', }
-#     return redirect(reverse('camera:index'))
+def capture_10s_video(request, camera=picamera.PiCamera(resolution=(1024, 768))):
+    video = camera.start_recording('apps/camera/static/images/video.h264')
+    sleep(2)
+    video.stop_recording()
+    context = {'video': 'images/video.h264',
+               'caption': 'Here is your video clip!', }
+    return redirect(reverse('camera:index'))
 
 
 # def video_preview(request, camera=picamera.PiCamera(resolution=(1024, 768))):
