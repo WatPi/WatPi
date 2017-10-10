812762fa948c208d71a67208ab1d44aefbe97656
Maki Roggers <18458620+makiroggers@users.noreply.github.com>
2017-10-10 12:43:14 -0700

    Remove unnecessary code, working camera buttons and video recording to connect with dashboard. (#37)
    * Updates to camera files.
    
    * Updated .gitignore to ignore experimental files.
    
    * Removed video streaming view.
    
    * Added extra buttons.
    
    * Added video button.
    
    * Bug fixes.

--- watpi/apps/camera/views.py
+++ watpi/apps/camera/views.py
@@ -4,44 +4,50 @@ from __future__ import unicode_literals
 from django.shortcuts import HttpResponse, redirect, render
 from django.urls import reverse
 from time import sleep
+
 import picamera
+import sys
 
 
 def index(request):
-    response = "Placeholder to verify rover app creation."
-    context = {'response': response, 
-               'image': 'images/image2.jpg', 
+    context = {'image_sm': 'images/image2.jpg', 
+               'image_lg': 'images/image3.jpg',
                'caption': 'Here is your image!', }
     return render(request, 'camera_index.html', context=context)
 
 
-def snap_photo(request, camera=picamera.PiCamera(resolution=(1024, 768))):
-    image = camera.capture('apps/camera/static/images/image2.jpg', resize=(320, 240))
-    context = {'image': 'images/image2.jpg',
-               'caption': 'Here is your image!', }
+def snap_photo(request):
+    camera = picamera.PiCamera(resolution=(1024, 768))
+    image = camera.capture('apps/camera/static/images/image_sm.jpg', resize=(320, 240))
+    context = {'image': 'images/image_sm.jpg',
+               'caption': 'Here is your smaller image!', }
+    camera.close()
     return redirect(reverse('camera:index'))
 
 
 def snap_lg_photo(request):
     camera = picamera.PiCamera(resolution=(1024, 768))
-    image = camera.capture('image.jpg', resize=(800, 600))
-    context = {'image': image,
-               'caption': 'Here is your smaller image!', }
-    return render(request, 'camera_index.html', context=context)
+    image = camera.capture('apps/camera/static/images/image_lg.jpg', resize=(800, 600))
+    context = {'image': 'images/image_lg.jpg',
+               'caption': 'Here is your bigger image!', }
+    camera.close()
+    return redirect(reverse('camera:index'))
 
 
 def capture_10s_video(request):
-    camera = picamera.PiCamera(1024, 768)
-    video = camera.start_recording('video.h264')
+    camera = picamera.PiCamera(resolution=(1024, 768))
+    video = camera.start_recording('apps/camera/static/images/video.h264')
     sleep(10)
-    video.stop_recording()
-    context = {'video': video,
+    camera.stop_recording()
+    camera.close()
+    context = {'video': 'images/video.h264',
                'caption': 'Here is your video clip!', }
-    return render(request, 'camera_index.html', context=context)
+    return redirect(reverse('camera:index'))
 
 
-def video_preview(request):
-    camera.start_preview()
-    camera.vflip = True
-    camera.hflip = True
-    camera.brightness = 60
+# def video_preview(request, camera=picamera.PiCamera(resolution=(1024, 768))):
+#     camera.start_preview()
+#     camera.vflip = True
+#     camera.hflip = True
+#     camera.brightness = 60
+#     return redirect(reverse('camera:index'))
