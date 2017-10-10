779e5ca40f2c99f0a8c032e80d89c74224435368
makiroggers <makiroggers@gmail.com>
2017-10-10 11:20:32 -0700

    Removed unneeded import.

--- watpi/apps/camera/views.py
+++ watpi/apps/camera/views.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import HttpResponse, redirect, render
 from django.urls import reverse
 from time import sleep
-from server import *
+
 import picamera
 import sys
 
