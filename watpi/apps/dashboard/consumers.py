f41748307968122e4695814fd69ebb05b56eb4ad
Lex Myers <lex.myers@gmail.com>
2017-10-11 15:14:17 -0700

    got image to go through websockets

--- watpi/apps/dashboard/consumers.py
+++ watpi/apps/dashboard/consumers.py
@@ -4,15 +4,11 @@ logging.basicConfig(level=logging.INFO)
 
 def ws_connect(message):
     message.reply_channel.send({"accept": True})
-    Group("survey").add(message.reply_channel)
+    Group("dash").add(message.reply_channel)
 
 def ws_message(message):
-    logging.info(message.content['text'])
-    # in_msg = json.loads(message.content['text'])
-    # in_msg['text']['random'] = random.randint(1,1000)
-    # out_msg = json.dumps(in_msg)
-    # logging.info(out_msg)
+    # logging.info(len(message.content['text']))
     out_msg = message.content['text']
-    Group("survey").send({
+    Group("dash").send({
         "text": out_msg,
     })
--- watpi/apps/dashboard/static/js/send_data.js
+++ watpi/apps/dashboard/static/js/send_data.js
@@ -20,33 +20,36 @@ $(document).ready(function() {
   var socket = new WebSocket("ws://" + window.location.host + "/dashboard/");
   $('#left-side').click(function(event) {
     event.preventDefault();
-    // var filename = '../images/dashboard/group_selfie.jpg';
-    // var img = $('<img />').attr('src', '../images/dashboard/group_selfie.jpg');
-    // var img = "nothing here";
-    var img = getBase64Image($('#grpslf'));
-    // img = fetch($('#grpslf'))
-    //   .then(function(response){
-    //     return response.blob();
-    //   })
-    console.log(img);
+    var src1 = '/static/images/dashboard/group_selfie.jpg';
+    var src2 = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=="
+    var imgtag = '<img src="' + src1 + '>"';
+    // $imgsrc = $('<img />').attr('src', src2)
+    // $(imgtag).on('load',  function() {
+    // });
+    // console.log(this)
+    // console.log($(this))
+    var img = getBase64Image($('#store')[0]);
+    console.log($('#store'))
+    // var img = "iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=="
     var sendIt = JSON.stringify({
       "text": {
         "image": img,
       }
     });
     socket.send(sendIt);
+    // imgtag.attr('height', 200);
   });
   socket.onmessage = function(event) {
     var text = JSON.parse(event.data).text;
     if(text){
-      var out = {
-        "image": text.image,
-      }
-      var outText = "<p>You emitted the following information to the server: ";
-      outText += "</p>";
-      console.log(out);
+      var src = "iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==";
+      var img = $('<img />').attr('src', 'data:image/png;base64,' + text.image);
+      console.log(text);
+      // var outText = "<p>You emitted the following information to the server: ";
+      // outText += "</p>";
+      // console.log(out);
       // outText += JSON.stringify(out) + "</p>";
     }
-    $('#emit').html(outText);
+    $('#emit').append(img);
   }
 })
