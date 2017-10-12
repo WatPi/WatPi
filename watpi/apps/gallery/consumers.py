65753ad24d01a2e6dd590bad3eb68ea87d8c966d
ayuspark <lola.luyaowang@gmail.com>
2017-10-12 15:49:46 -0700

    socket now sends latest photo taken from dashboard to gallery

--- watpi/apps/gallery/consumers.py
+++ watpi/apps/gallery/consumers.py
@@ -1,5 +1,7 @@
-from channels import Group
 import logging, json, base64, os
+from channels import Group
+from ..dashboard.models import *
+
 logging.basicConfig(level=logging.INFO)
 
 def ws_connect(message):
@@ -11,7 +13,10 @@ def ws_message(message):
     print os.getcwd()
 
     # TODO: change path
-    with open('apps/dashboard/static/dashboard/images/group_selfie.jpg', 'rb') as imgfile:
+    lasted_photo_name = Photo.objects.order_by('-time_created').first().name
+    latest_photo_path = 'apps/dashboard/static/dashboard/images/' + lasted_photo_name
+
+    with open(latest_photo_path, 'rb') as imgfile:
         encoded = base64.b64encode(imgfile.read())
     out = {
         "image": encoded
