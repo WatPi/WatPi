9f6296785a3b8ae85f1ef70a87ed3c4bfb66413f
makiroggers <makiroggers@gmail.com>
2017-10-12 01:20:54 -0700

    Added google vision api code to fetch annotations.

--- watpi/apps/camera/views.py
+++ watpi/apps/camera/views.py
@@ -1,13 +1,10 @@
-# TODO:
-# # -*- coding: utf-8 -*-
-# from __future__ import unicode_literals
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-# from django.shortcuts import HttpResponse, redirect, render
-# from django.urls import reverse
-# from time import sleep
-
+from django.shortcuts import HttpResponse, redirect, render
+from django.urls import reverse
+from time import sleep
 # import picamera
-# import sys
 
 
 # def index(request):
@@ -46,9 +43,33 @@
 #     return redirect(reverse('camera:index'))
 
 
-# # def video_preview(request, camera=picamera.PiCamera(resolution=(1024, 768))):
-# #     camera.start_preview()
-# #     camera.vflip = True
-# #     camera.hflip = True
-# #     camera.brightness = 60
-# #     return redirect(reverse('camera:index'))
+# def video_preview(request, camera=picamera.PiCamera(resolution=(1024, 768))):
+#     camera.start_preview()
+#     camera.vflip = True
+#     camera.hflip = True
+#     camera.brightness = 60
+#     return redirect(reverse('camera:index'))
+
+import io
+import os
+from google.cloud import vision
+from google.cloud.vision import types
+    
+def annotate_image(request):
+    image_labels = []
+    client = vision.ImageAnnotatorClient()
+    file_name = 'camera/static/images/image_sm.jpg'
+
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
+
+    image = types.Image(content=content)
+
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+
+    print('Labels: ')
+    for label in labels:
+        print(label.description)
+        image_labels.append(label.description)
+    return HttpResponse(content=str(image_labels))
