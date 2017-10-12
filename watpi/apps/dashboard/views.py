0e6b8649ef2a436f80890e49bf935aba6cf7b4aa
ayuspark <lola.luyaowang@gmail.com>
2017-10-12 12:26:18 -0700

    changed path to save and delete photo locally

--- watpi/apps/dashboard/views.py
+++ watpi/apps/dashboard/views.py
@@ -60,13 +60,13 @@ def take_photo(request):
     new_name = 'img_' + str(timezone.now())[:26] + '.jpg'
     new_name = new_name.replace(' ', '_')
     new_time_created = timezone.now()
-    addr = 'apps/dashboard/static/images/' + new_name
+    addr = 'apps/dashboard/static/dashboard/images/' + new_name
 
     if number_of_photos_stored == 10:
         # rewrite the oldest photo, cap the number of photos at 10
         photo_to_rw = Photo.objects.order_by('time_created').first()
         photo_to_delete = photo_to_rw.name
-        path_to_delete = 'apps/dashboard/static/images/' + photo_to_delete
+        path_to_delete = 'apps/dashboard/static/dashboard/images/' + photo_to_delete
         os.remove(path_to_delete) 
 
         photo_to_rw.update()
